d9833c43b1d28dcf0fbeb5dd148b453ccd33ee39
Đăng Lê <danglh23406@st.uel.edu.vn>
2026-05-22 15:49:42 +0700

    constants

---
+++ frontend/src/constants/priority.js
@@ -0,0 +1,5 @@
+export const PRIORITY = {
+  HIGH: "Cao",
+  MEDIUM: "Trung bình",
+  LOW: "Thấp",
+};
---
+++ frontend/src/constants/roles.js
@@ -0,0 +1,6 @@
+export const ROLES = {
+  REQUESTER: "requester",
+  L1: "l1",
+  L2: "l2",
+  MANAGER: "manager",
+};
