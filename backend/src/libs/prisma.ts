ce25802b4f9c602171a3c5eead2d059be1e587ef
phthang-PX1 <phthang0872@gmail.com>
2026-05-23 11:27:21 +0700

    Backend: cập nhật 4 API ticket

--- backend/src/libs/prisma.ts
+++ backend/src/libs/prisma.ts
@@ -1,11 +1,10 @@
+// src/libs/prisma.ts
 import { PrismaClient } from '@prisma/client';
 
-// Đoạn code này đảm bảo khi bạn code ở môi trường Development, 
-// mỗi lần lưu file (Nodemon/Ts-node-dev reset) hệ thống không bị tạo lập vô số kết nối thừa.
 const globalForPrisma = global as unknown as { prisma: PrismaClient };
 
 export const prisma = globalForPrisma.prisma || new PrismaClient({
-  log: ['query', 'info', 'warn', 'error'], // Hiện câu lệnh SQL dưới Terminal để bạn dễ debug
+  log: ['query', 'info', 'warn', 'error'],
 });
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
